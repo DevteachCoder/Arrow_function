ee113b71ebe11de62186fea2872c1348270f9a98
Doniyorbek <142497625+DevteachCoder@users.noreply.github.com>
2026-02-07 15:25:52 +0500

    Add files via upload

---
+++ 4.2-lesson/arrow function/script.js
@@ -0,0 +1,50 @@
+// //Function as values
+
+
+
+
+// let sum = function() {
+//     console.log("salom");
+// }
+
+// if(false) {
+//     sum = function() {
+//         console.log("Good Bye")
+
+//     }
+// }
+
+// sum();
+
+
+//Declaration notation//
+
+// function summ() {
+//    console.log('salom') 
+// }
+
+// function summ(num1,num2) {
+//     return num1+num2;
+// }
+// console.log(summ(12,32));
+
+// function sqrt(x) {
+//     return x*x;
+// }
+// console.log(sqrt(10));
+
+// let age = prompt("What is your age!",18);
+
+// if age >=18
+
+let radius = 1;
+let x = 1;
+ley = y;
+
+const circle = {
+    radius:1,
+    location: {
+        x:1,
+        y:1
+    }
+}
